8e2b3125614e09613d958285cea5c51336cc9001
muschellij2 <muschellij2@gmail.com>
2015-04-08 17:26:07 -0400

    added matlabr

--- White_Matter_Segmentation_in_R/White_Matter_Segmentation_in_R.R
+++ White_Matter_Segmentation_in_R/White_Matter_Segmentation_in_R.R
@@ -28,6 +28,8 @@ img.stub = nii.stub(img.name)
 ## ----biascorrection, cache=TRUE------------------------------------------
 n4img = bias_correct( img.name, correction = "N4", 
                       outfile = paste0(img.stub, "_N4.nii.gz") )
+
+## ----biascorrection_plot, cache=FALSE------------------------------------
 ortho2(n4img)
 
 ## ----bet, dependson="biascorrection"-------------------------------------
@@ -44,7 +46,7 @@ bet = fslbet_robust(img = n4img,
                     outfile = "SUBJ0001-01-MPRAGE_N4_BET", 
                     correct = FALSE)
 
-## ----bet_plot, dependson="bet"-------------------------------------------
+## ----bet_plot, dependson="bet", cache=FALSE------------------------------
 ortho2(n4img, bet > 0, 
        col.y=alpha("red", 0.5))
 
@@ -53,11 +55,11 @@ fast = fast(file = bet,
             outfile = paste0(img.stub, "_BET_FAST"), 
             opts = '-N')
 
-## ----fast_plot, dependson="fast"-----------------------------------------
+## ----fast_plot, dependson="fast", cache=FALSE----------------------------
 ortho2(bet, fast == 3, 
        col.y=alpha("red", 0.5))
 
-## ----fast_plot_csf_gm, dependson="fast"----------------------------------
+## ----fast_plot_csf_gm, dependson="fast", cache=FALSE---------------------
 ortho2(bet, fast == 1, col.y=alpha("red", 0.5), text="CSF Results")
 ortho2(bet, fast == 2, col.y=alpha("red", 0.5), text="Gray Matter\nResults")
 
