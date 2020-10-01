7171de6b0d0f45197d6b747db0b843f2b8e3770d
muschellij2 <muschellij2@gmail.com>
2020-10-01 10:32:01 -0400

    updated the wish about grad school

---
+++ Some_things_I_wish_I_knew_about_Grad_School/Some_things_I_wish_I_knew_about_Grad_School.R
@@ -0,0 +1,4 @@
+## ----label=opts, results='hide', echo=FALSE, message = FALSE, warning=FALSE----------------------------------
+library(knitr)
+opts_chunk$set(echo = TRUE, prompt = FALSE, message = FALSE, warning = FALSE, comment = "", results = "hide")
+
